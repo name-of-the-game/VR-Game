e6e1c0ecc1859fd25fab6c26138c9ce6cb7b954a
elifKurtay <47862570+elifKurtay@users.noreply.github.com>
2019-08-01 15:26:15 +0200

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 move;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Left))
+        {
+            move = new Vector3(-1f, 0f, 0f);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Right))
+        {
+            move = new Vector3( 1f, 0f, 0f);
+        }
+
+        transform.position = move;
+    }
+}
+
---
+++ Spawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] cubes;
+    public Transform[] points;
+    public float beat;
+    private float timer;
+    bool isActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( isActive )
+        {
+            isActive = false;
+            //if (timer > beat)
+            //{
+                var postion = Random.Range(0, 4);
+                var point = 0;
+
+                if (postion == 0)
+                {
+                    point = Random.Range(0, 7);
+                }
+                else if (postion == 1)
+                {
+                    point = Random.Range(2, 9);
+                }
+                else if (postion == 2)
+                {
+                    point = Random.Range(4, 11);
+                }
+                else
+                {
+                    point = Random.Range(0, 11);
+                }
+
+                GameObject cube = Instantiate(cubes[postion], points[point]);
+                cube.transform.localPosition = Vector3.zero;
+                //timer -= beat;
+            //}
+
+            //timer += Time.deltaTime;
+            StartCoroutine("DoCheck");
+        }
+        
+    }
+
+    IEnumerator DoCheck()
+    {
+        yield return new WaitForSeconds(2.0f);
+        isActive = true;
+    }
+}
